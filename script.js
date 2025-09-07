98fdb45e7141ff30e3a8f55fb5d31c9d55f9030f
Raju Pusti <rajupusti@yahoo.com>
2025-09-07 11:07:53 +0000

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+let wpm = document.getElementById("wpm");
+let accuracy = document.getElementById("accuracy");
+let timer = document.getElementById("timer");
+let errors = document.getElementById("errors");
+let typingText = document.getElementById("typing-text");
+let resetBtn = document.getElementById("reset-btn");
+
+ console.log(wpm, accuracy, timer,errors,typingText,resetBtn);
+  let text = ""
